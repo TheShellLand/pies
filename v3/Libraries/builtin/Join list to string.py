e27b20e897182ceb47958d5b346b5f2d1fec6c37
eric <naisanza@gmail.com>
2015-07-29 15:57:50 -0400

    Added additional methods

--- v3/Libraries/builtin/Join list to string.py
+++ v3/Libraries/builtin/Join list to string.py
@@ -1,2 +1,18 @@
 a = ['a', 'b', 'c', 'd']
 ''.join(a)
+
+#####
+
+# create a concatenated string from 0 to 19 (e.g. "012..1819")
+nums = [str(n) for n in range(20)]
+print "".join(nums)
+
+
+#####
+
+foo = 'foo'
+bar = 'bar'
+
+foobar = foo + bar  # This is good
+foo += 'ooo'  # This is bad, instead you should do:
+foo = ''.join([foo, 'ooo'])
