b7c70cf2245f37f1daea418f9267364115aaadba
eric <naisanza@gmail.com>
2015-08-03 11:31:09 -0400

    New file

---
+++ v3/Libraries/subprocess/Popen/stdout stderr.py
@@ -0,0 +1,4 @@
+
+# stdin, stdout and stderr specify the executed programs’ standard input, standard output and standard error
+# file handles, respectively. Valid values are PIPE, an existing file descriptor (a positive integer), an
+# existing file object, and None.
