f6ecb5df49e6e2e65268321b58054b43bfc0619f
eric <naisanza@gmail.com>
2015-08-03 11:26:24 -0400

    New file

---
+++ v3/Libraries/subprocess/Popen/New console.py
@@ -0,0 +1,6 @@
+from sys import executable
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+Popen([executable, 'script.py'], creationflags=CREATE_NEW_CONSOLE)
+
+input('Enter to exit from this launcher script...')
