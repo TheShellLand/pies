e50c7c44612f6a8400c0657fb8ad7f297c49120e
eric <naisanza@gmail.com>
2015-08-18 15:16:40 -0400

    Empire of Code bird language

--- v3/Libraries/pip/Upgrade all packages.py
+++ v3/Libraries/pip/Upgrade all packages.py
@@ -1,4 +1,8 @@
-import pip, subprocess
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import pip
+import subprocess
 
 for distribution in pip.get_installed_distributions():
-    subprocess.call("python -m pip install --upgrade " + distribution.project_name, shell=True)
+    subprocess.call('python3 -m pip install --upgrade ' + distribution.project_name, shell=True)
---
+++ v3/scripts/testing/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import pip, subprocess
+
+subprocess.call('python3 --version', shell=1)
+
