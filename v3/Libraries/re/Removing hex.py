035c876b9a3276fc5ccd378498e49d15b0502eb0
eric <naisanza@gmail.com>
2015-10-01 14:32:42 -0400

    Finished Removing hex.py

--- v3/Libraries/re/Removing hex.py
+++ v3/Libraries/re/Removing hex.py
@@ -7,21 +7,21 @@
 '''
 You can remove all non-word characters in the following way:
 
->>> re.sub(r'[^\w]', '', 'MagX\x00\x00\x00\x08\x01\x008\xe6\x7f')
+#>>> re.sub(r'[^\w]', '', 'MagX\x00\x00\x00\x08\x01\x008\xe6\x7f')
 'MagX8'
 
 The regex [^\w] will match any character that is not a letter, digit, or underscore. By providing that regex in re.sub with an empty string as a replacement you will delete all other characters in the string.
 
 Since there may be other characters you want to keep, a better solution might be to specify a larger range of characters that you want to keep that excludes control characters. For example:
 
->>> re.sub(r'[^\x20-\x7e]', '', 'MagX\x00\x00\x00\x08\x01\x008\xe6\x7f')
+#>>> re.sub(r'[^\x20-\x7e]', '', 'MagX\x00\x00\x00\x08\x01\x008\xe6\x7f')
 'MagX8'
 
 Or you could replace [^\x20-\x7e] with the equivalent [^ -~], depending on which seems more clear to you.
 
 To exclude all characters after this first control character just add a .*, like this:
 
->>> re.sub(r'[^ -~].*', '', 'MagX\x00\x00\x00\x08\x01\x008\xe6\x7f')
+#>>> re.sub(r'[^ -~].*', '', 'MagX\x00\x00\x00\x08\x01\x008\xe6\x7f')
 'MagX'
 
 '''
