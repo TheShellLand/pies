f0a1445c8f65f948b6d469355b00e3b2f70b27c3
eric <naisanza@gmail.com>
2015-11-08 19:05:41 -0500

    Sorting items in a list

---
+++ v3/Libraries/builtin/sorted/Sorting List.py
@@ -0,0 +1,8 @@
+
+
+lst = ['Stem', 'constitute', 'Sedge', 'Eflux', 'Whim', 'Intrigue']
+
+sorted(lst)
+sorted(lst, key=str.lower)
+sorted(lst, reverse=True)
+sorted(lst, key=str.lower, reverse=True)
