9d416a5d3c55fae7f498de47fa02e0cdfe896b47
eric <naisanza@gmail.com>
2015-11-08 19:23:56 -0500

    Storing tuples in a dictionary

--- v3/Libraries/builtin/lists-dictionaries/dict.py
+++ v3/Libraries/builtin/lists-dictionaries/dict.py
@@ -8,3 +8,14 @@ Zed
 >>> stuff['city'] = "San Francisco"
 >>> print stuff['city']
 San Francisco
+
+
+>>> dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+{'sape': 4139, 'jack': 4098, 'guido': 4127}
+
+>>> {x: x**2 for x in (2, 4, 6)}
+{2: 4, 4: 16, 6: 36}
+
+>>> dict(sape=4139, guido=4127, jack=4098)
+{'sape': 4139, 'jack': 4098, 'guido': 4127}
+
