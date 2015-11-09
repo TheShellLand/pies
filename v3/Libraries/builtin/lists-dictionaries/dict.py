bc01503b4622c22337e480e7960b633711da365d
eric <naisanza@gmail.com>
2015-11-08 19:05:16 -0500

    Storing tuples in a dictionary

---
+++ v3/Libraries/builtin/lists-dictionaries/dict.py
@@ -0,0 +1,10 @@
+>>> stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 + 2}
+>>> print stuff['name']
+Zed
+>>> print stuff['age']
+39
+>>> print stuff['height']
+74
+>>> stuff['city'] = "San Francisco"
+>>> print stuff['city']
+San Francisco
