5b35166d8bd8bd7c40ace5e181390bb04449397e
eric <naisanza@gmail.com>
2015-11-27 16:29:07 -0500

    Readability

--- v3/Libraries/builtin/statement/try except finally.py
+++ v3/Libraries/builtin/statement/try except finally.py
@@ -13,6 +13,12 @@ finally:
 
 
 '''
-The code in block-1 is executed. If the code raises an exception, the various except blocks are tested: if the exception is of class Exception1, handler-1 is executed; otherwise if it's of class Exception2, handler-2 is executed, and so forth. If no exception is raised, the else-block is executed.
-No matter what happened previously, the final-block is executed once the code block is complete and any raised exceptions handled. Even if there's an error in an exception handler or the else-block and a new exception is raised, the code in the final-block is still run.
+The code in block-1 is executed. If the code raises an exception, the various except blocks are tested: if the
+exception is of class Exception1, handler-1 is executed; otherwise if it's of class Exception2, handler-2 is
+executed, and so forth. If no exception is raised, the else-block is executed.
+
+No matter what happened previously, the final-block is executed once the code block is complete and any raised
+exceptions handled. Even if there's an error in an exception handler or the else-block and a new exception is
+raised, the code in the final-block is still run.
 '''
+
