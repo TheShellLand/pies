82f07c0d61609278d5d5ae9794800d52557a8c4b
eric <naisanza@gmail.com>
2015-11-28 01:35:32 -0500

    user input password prompt

--- v3/Libraries/getpass/getpass.py
+++ v3/Libraries/getpass/getpass.py
@@ -1,10 +1,7 @@
-
-
-
+import getpass
 
 # refer: https://docs.python.org/3.4/library/getpass.html
 
 
-
 getpass.getpass(prompt='Password: ', stream=None)
-getpass.getuser()
+#CurrentUser = getpass.getuser()
