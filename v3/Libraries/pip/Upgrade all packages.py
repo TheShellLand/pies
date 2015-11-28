8275d7904176989f657eca9a3b0a4d78b01ebb6e
eric <naisanza@gmail.com>
2015-11-28 01:19:30 -0500

    Use python

--- v3/Libraries/pip/Upgrade all packages.py
+++ v3/Libraries/pip/Upgrade all packages.py
@@ -5,4 +5,4 @@ import pip
 import subprocess
 
 for distribution in pip.get_installed_distributions():
-    subprocess.call('python3 -m pip install --upgrade ' + distribution.project_name, shell=True)
+    subprocess.call('python -m pip install --upgrade ' + distribution.project_name, shell=True)
