c0a16a659acd8ceef63d4b6408bb60667d14bcd5
eric <naisanza@gmail.com>
2015-11-30 16:20:38 -0500

    AES Crypto.Cipher requires padding if size of input is not a multiple of BLOCK_SIZE

---
+++ v3/Libraries/Crypto/AES_Padding_BLOCKSIZE.py
@@ -0,0 +1,6 @@
+
+# Padding needed if length of input is not a multiple of BLOCK_SIZE
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s : s[:-ord(s[len(s)-1:])]
