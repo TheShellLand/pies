d60a7ca6db5e9e1f05f7e69cffaac7ec3dbc2781
eric <naisanza@gmail.com>
2015-11-30 16:13:46 -0500

    Added AES Crypto.Cipher example

---
+++ v3/Libraries/Crypto/AES_DecryptSample.py
@@ -0,0 +1,14 @@
+
+# refer: https://pypi.python.org/pypi/pycrypto/2.6.1
+
+from Crypto.Cipher import AES
+
+obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+message = "The answer is no"
+ciphertext = obj.encrypt(message)
+ciphertext
+# '\xd6\x83\x8dd!VT\x92\xaa`A\x05\xe0\x9b\x8b\xf1'
+
+obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+obj2.decrypt(ciphertext)
+# 'The answer is no'
