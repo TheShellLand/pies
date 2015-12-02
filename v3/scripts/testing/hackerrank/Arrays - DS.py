c630c3f4cbd83a8d713224e6e880a2e15ee69b55
eric <naisanza@gmail.com>
2015-12-02 17:13:40 -0500

    hackerrank arrays ds

--- v3/scripts/testing/hackerrank/Arrays - DS.py
+++ v3/scripts/testing/hackerrank/Arrays - DS.py
@@ -5,8 +5,16 @@ LEN = int(input().strip())
 ARRAY = input().strip()
 
 
-if LEN == len(re.findall('[0-9]', ARRAY)):
-    print(ARRAY[::-1])
+ARRAY_list = ARRAY.split(' ')
+index = LEN
+
+
+#print('Length of array:', LEN)
+
+
+if LEN == len(re.findall('[0-9]+', ARRAY)):
+    print(' '.join(ARRAY_list[::-1]))
 else:
     print('Length not equal to array. Exiting')
+    exit(1)
 
