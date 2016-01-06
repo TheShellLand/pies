d42361c342711f7f952982b183c2f8bcede34554
Eric Jaw <naisanza@gmail.com>
2016-01-06 14:53:23 -0500

    added range for keys

--- v3/scripts/yubikey-bruting.py
+++ v3/scripts/yubikey-bruting.py
@@ -16,10 +16,11 @@ from subprocess import call, Popen, PIPE
 
 yubikeyPersonalize = 'bin/ykpersonalize.exe'
 ykProfile = '-2'    # choose second profile
-ykKey = '-c' + '0011223344'    # 6 bytes hex 00 11 22 33 44 55
+ykKey = '-c' + '00112233'    # 6 bytes hex 00 11 22 33 44 55
 ykPrompt = '-y'     # always commit
 
-bruteKeys = [0,0,0,0,1,1,1,1]  # known values
+#bruteKeys = [8,8,8,8,9,9,9,9]  # known values
+bruteKeys = range(0, 10)       # known values
 
 totalKeys = 16**6
 totalKeysKnown = 16**4
@@ -33,6 +34,7 @@ timeStart = time.time()
 
 
 #  for a in itertools.permutations([8,8,8,8,0,0,0,0], r=4): print(a[0], a[1], a[2], a[3], sep='')
+#  for a in itertools.permutations(range(0, 10), r=4): print(a[0], a[1], a[2], a[3], sep='')
 
 totalKeys = 0
 for a in itertools.permutations(bruteKeys, r=4):
@@ -43,18 +45,13 @@ for a in itertools.permutations(bruteKeys, r=4):    # all permutations of [8,8,8
     bruteKeysCount = bruteKeysCount - 1
 
     key1, key2, key3, key4 = a      # convert tuple to int
-
     ykKeyGuess = ykKey + str(key1) + str(key2) + str(key3) + str(key4)
-    #print('Trying key:', ykKeyGuess[2:])
 
     #call([yubikeyPersonalize, ykProfile, ykKeyGuess, ykPrompt, '-z'])
 
     p = Popen([yubikeyPersonalize, ykProfile, ykKeyGuess, ykPrompt, '-z'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate()
     rc = p.returncode
-
-    #print(output.decode())
-    #print(err.decode())
 
     errEncoding = chardet.detect(err)
     keyFail = re.findall('Yubikey core error: write error', err.decode(errEncoding['encoding']))
