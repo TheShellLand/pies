271d03870b3b6ced94b9e4773e0f154ec9e5f413
Eric Jaw <naisanza@gmail.com>
2016-01-06 09:14:01 -0500

    inline replace characters

---
+++ v3/Libraries/builtin/replace/inline replace characters.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf8 -*-
+
+# '0c a8 f0 d6 02 00 00 00 00 d0 1c d1 10 d2 00 d3 00 d7 01 d4 78 20 ff'.replace(' ', '').decode('hex')
+
+print('0c a8 f0 d6 02 00 00 00 00 d0 1c d1 10 d2 00 d3 00 d7 01 d4 78 20 ff'.replace(' ', ''))
