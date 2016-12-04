0fde0985d5fe04f8682a1dbe4fe3d93e683edee6
Eric Jaw <naisanza@gmail.com>
2016-12-04 04:31:43 -0500

    Example elasticsearch client

---
+++ v3/Libraries/elasticsearch/elasticclient.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+Example from https://elasticsearch-py.readthedocs.io/en/master/
+"""
+
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+# todo: need to unzip files
+
+
+es = Elasticsearch()
+
+doc = {
+    'author': 'kimchy',
+    'text': 'Elasticsearch: cool. bonsai cool.',
+    'timestamp': datetime.now(),
+}
+res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
+print(res['created'])
+
+res = es.get(index="test-index", doc_type='tweet', id=1)
+print(res['_source'])
+
+es.indices.refresh(index="test-index")
+
+res = es.search(index="test-index", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
