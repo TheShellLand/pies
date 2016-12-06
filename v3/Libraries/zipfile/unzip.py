e87c6be4a85d894d570fcda888d57d5cb323fa03
Eric Jaw <naisanza@gmail.com>
2016-12-06 02:33:37 -0500

    Added Unzip to memory

--- v3/Libraries/zipfile/unzip.py
+++ v3/Libraries/zipfile/unzip.py
@@ -7,3 +7,11 @@ import zipfile
 zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
 zip_ref.extractall(directory_to_extract_to)
 zip_ref.close()
+
+
+"""
+Unzip to memory
+"""
+def extract_zip(input_zip):
+    input_zip=ZipFile(input_zip)
+    return {name: input_zip.read(name) for name in input_zip.namelist()}
