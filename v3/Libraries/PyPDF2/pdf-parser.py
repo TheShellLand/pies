f39aa8e3fb14f473b03a45bf1cfaaef08874cc06
eric <naisanza@gmail.com>
2017-01-23 04:14:32 -0500

    PDF parser

--- v3/Libraries/PyPDF2/pdf-parser.py
+++ v3/Libraries/PyPDF2/pdf-parser.py
@@ -1,22 +1,19 @@
-
-
 # Examples from https://automatetheboringstuff.com/chapter13/
 
-"""
-   >>> import PyPDF2
-   >>> pdfFileObj = open('meetingminutes.pdf', 'rb')
-   >>> pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-❶ >>> pdfReader.numPages
-   19
-❷ >>> pageObj = pdfReader.getPage(0)
-❸ >>> pageObj.extractText()
-   'OOFFFFIICCIIAALL BBOOAARRDD MMIINNUUTTEESS Meeting of March 7, 2015
-   \n     The Board of Elementary and Secondary Education shall provide leadership
-   and create policies for education that expand opportunities for children,
-   empower families and communities, and advance Louisiana in an increasingly
-   competitive global market. BOARD of ELEMENTARY and SECONDARY EDUCATION '
-"""
 
+"""
+#    >>> import PyPDF2
+#    >>> pdfFileObj = open('meetingminutes.pdf', 'rb')
+#    >>> pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# ❶ >>> pdfReader.numPages
+#    19
+# ❷ >>> pageObj = pdfReader.getPage(0)
+# ❸ >>> pageObj.extractText()
+#    'OOFFFFIICCIIAALL BBOOAARRDD MMIINNUUTTEESS Meeting of March 7, 2015
+#    \n     The Board of Elementary and Secondary Education shall provide leadership
+#    and create policies for education that expand opportunities for children,
+#    empower families and communities, and advance Louisiana in an increasingly
+#    competitive global market. BOARD of ELEMENTARY and SECONDARY EDUCATION '
+"""
 
 import PyPDF2
-
