0979bdad19739b2cf75382d25b79f0d77f86a6f6
eric <naisanza@gmail.com>
2017-04-03 19:06:27 -0400

    Upgrade pip packages

---
+++ v3/Libraries/pip/Upgrade.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import pip
+import subprocess
+
+try:
+    for distribution in pip.get_installed_distributions():
+        subprocess.call('python3 -m pip install --upgrade ' + distribution.project_name, shell=True)
+except OSError:
+    print('Permission denied')
